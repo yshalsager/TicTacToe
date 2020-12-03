b22c2a6f85a43d0f4a4a691f9c52d24af9360b64
yshalsager <ysh-alsager@hotmail.com>
2020-12-03 13:50:13 +0200

    Create menu in home fragment
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- app/src/main/java/me/yshalsager/tictactoe/HomeFragment.kt
+++ app/src/main/java/me/yshalsager/tictactoe/HomeFragment.kt
@@ -1,13 +1,17 @@
 package me.yshalsager.tictactoe
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import me.yshalsager.tictactoe.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,8 +27,21 @@ class HomeFragment : Fragment() {
         binding.startButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_gameFragment)
         )
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item, view!!.findNavController()
+        )
+                || super.onOptionsItemSelected(item)
     }
 
 }
