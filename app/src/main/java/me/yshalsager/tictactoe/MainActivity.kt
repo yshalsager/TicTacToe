e9a2c5c99c1d7371f28b8e751fcf8cbcce5a9b7c
yshalsager <ysh-alsager@hotmail.com>
2020-12-03 15:15:48 +0200

    Add Navigation Drawer
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- app/src/main/java/me/yshalsager/tictactoe/MainActivity.kt
+++ app/src/main/java/me/yshalsager/tictactoe/MainActivity.kt
@@ -3,25 +3,37 @@ package me.yshalsager.tictactoe
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import me.yshalsager.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
+
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
-//        setContentView(binding.root)
+        // Initialize drawerLayout var from binding
+        drawerLayout = binding.drawerLayout
+
         val navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
     }
 
 }
